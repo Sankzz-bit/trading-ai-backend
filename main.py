cd3a3240c236340a7a44e9ce53b7127fe446b93a
Sankzz-bit <s09122105@gmail.com>
2026-03-25 14:24:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/api/analysis")
 def get_analysis():
     try:
